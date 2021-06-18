a8f09767e61c5be634f807353561a8641599beb0
Anstalex <a.v.bakina@mail.ru>
2021-06-19 00:53:31 +0600

    feat(scope) fix
    #lesson17

--- TASK/script.js
+++ TASK/script.js
@@ -4,24 +4,32 @@ const pDayWeek = document.querySelector('.day-week')
 const pTime = document.querySelector('.time')
 const pNewYear = document.querySelector('.new-year')
 
-const date = new Date();
+let date = new Date();
 const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Четверг', 'Пятница', 'Суббота']
 const day = date.getDay()
-const time = date.toLocaleTimeString('en')
 const hours = date.getHours()
 const newYear = new Date('01 january 2022')
-let timeRemaining = newYear.getTime()- date.getTime()
-let dayRemaining = Math.floor(timeRemaining/86400/1000)
+let timeRemaining = newYear.getTime() - date.getTime()
+let dayRemaining = Math.floor(timeRemaining / 86400 / 1000)
 
-if ((hours >= 0 && hours <= 5)||(hours >= 23)){
+if ((hours >= 0 && hours <= 5) || (hours >= 23)) {
     pHello.innerText = 'Доброй ночи'
-}else if (hours >= 6 && hours <= 11){
+} else if (hours >= 6 && hours <= 11) {
     pHello.innerText = 'Доброе утро'
-}else if (hours >= 12 && hours <= 16){
+} else if (hours >= 12 && hours <= 16) {
     pHello.innerText = 'Добрый день'
-}else if (hours >= 17 && hours <= 22){
+} else if (hours >= 17 && hours <= 22) {
     pHello.innerText = 'Добрый вечер'
 }
-    pDayWeek.innerText = `Сегодня: ${week[day - 1]}`
+let time = date.toLocaleTimeString('en')
 pTime.innerText = `Текущее время: ${time}`
- pNewYear.innerText =` До нового года осталось ${dayRemaining} дней`
+
+let getTime=()=>{
+    date = new Date();
+    let time = date.toLocaleTimeString('en')
+    pTime.innerText = `Текущее время: ${time}`
+}
+pDayWeek.innerText = `Сегодня: ${week[day - 1]}`
+setInterval(getTime,1000)
+
+pNewYear.innerText = ` До нового года осталось ${dayRemaining} дней`
