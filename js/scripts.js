a88a6f5921c5e7c7522a1cbe9508030d5291a759
Anstalex <a.v.bakina@mail.ru>
2021-06-21 15:43:14 +0600

    feat(scope) esLint fix bags
    #Hardlesson18

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
-const html = document.querySelector('html')
+'use strict';
+const html = document.querySelector('html');
 
-window.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-
+window.addEventListener('DOMContentLoaded', () => {
 
     //Таймер
     let timer;
 
     function countTimer(deadline) {
-        let timerHours = document.getElementById('timer-hours');
-        let timerMinutes = document.getElementById('timer-minutes');
-        let timerSeconds = document.getElementById('timer-seconds');
-        let news = {}
+        const timerHours = document.getElementById('timer-hours');
+        const timerMinutes = document.getElementById('timer-minutes');
+        const timerSeconds = document.getElementById('timer-seconds');
+        const news = {};
 
         function getTimeRemaining() {
-            let dateStop = new Date(deadline).getTime();
-            let dateNow = new Date().getTime();
-            let timeRemaining = (dateStop - dateNow) / 1000;
-            let seconds = Math.floor(timeRemaining % 60);
-            let minutes = Math.floor((timeRemaining / 60) % 60);
-            let hours = Math.floor(timeRemaining / 60 / 60);
+            const dateStop = new Date(deadline).getTime();
+            const dateNow = new Date().getTime();
+            const timeRemaining = (dateStop - dateNow) / 1000;
+            const seconds = Math.floor(timeRemaining % 60);
+            const minutes = Math.floor((timeRemaining / 60) % 60);
+            const hours = Math.floor(timeRemaining / 60 / 60);
             news.timeRemaining = timeRemaining;
             news.hours = hours;
             news.minutes = minutes;
@@ -28,13 +27,13 @@ window.addEventListener('DOMContentLoaded', function () {
 
         function updateClock() {
             getTimeRemaining();
-            let addNull = (item) => {
+            const addNull = item => {
                 if (item < 10) {
-                    return '0' + item
+                    return '0' + item;
                 } else {
-                    return +item
+                    return +item;
                 }
-            }
+            };
             timerHours.innerText = addNull(news.hours);
             timerMinutes.innerText = addNull(news.minutes);
             timerSeconds.innerText = addNull(news.seconds);
@@ -59,11 +58,11 @@ window.addEventListener('DOMContentLoaded', function () {
 const handler = (item, event, callback) => {
     item.addEventListener(event, callback);
 };
-const handlerArray = function (items, event, callback) {
+const handlerArray = function(items, event, callback) {
     for (const key of items) {
-        key.addEventListener(event, callback)
+        key.addEventListener(event, callback);
     }
-}
+};
 
 const toggleMenu = () => {
     const btnMenu = document.querySelector('.menu');
@@ -78,9 +77,9 @@ const toggleMenu = () => {
         menu.classList.remove('active-menu');
     };
 
-    handler(document, 'click', (e)=>{
-        if (!e.target.closest('menu')&&(!e.target.closest('.menu'))) {
-            menuClose()
+    handler(document, 'click', e => {
+        if (!e.target.closest('menu') && (!e.target.closest('.menu'))) {
+            menuClose();
         }
     });
     handler(btnMenu, 'click', menuOpen);
@@ -96,41 +95,41 @@ toggleMenu();
 
 const togglePopup = () => {
     let count = '-20';
-    const popupContent = document.querySelector('.popup-content')
+    const popupContent = document.querySelector('.popup-content');
     const popup = document.querySelector('.popup');
     const popupBtn = document.querySelectorAll('.popup-btn');
     const popupClose = document.querySelector('.popup-close');
-    popup.style.transition = 'all 1s ease'
+    popup.style.transition = 'all 1s ease';
     let idAnimate;
 
     const popupShow = () => {
         popup.style.display = 'block';
         if ((count < 38) && (html.clientWidth > 768)) {
-            count+=2
+            count += 2;
             popupContent.style.left = `${count}%`;
             idAnimate = requestAnimationFrame(popupShow);
         } else {
-            cancelAnimationFrame(idAnimate)
+            cancelAnimationFrame(idAnimate);
         }
     };
 
     const popupHide = () => {
         if ((count >= 38 && count < 100) && (html.clientWidth > 768)) {
             idAnimate = requestAnimationFrame(popupHide);
-            count+=2
-            popupContent.style.left = `${count}%`
+            count += 2;
+            popupContent.style.left = `${count}%`;
         } else {
-            cancelAnimationFrame(popupHide)
-            popup.style.display = 'none'
+            cancelAnimationFrame(popupHide);
+            popup.style.display = 'none';
             count = -20;
         }
     };
 
-    handler(document,'click',(e)=>{
-        if (!e.target.closest('.popup-content')){
+    handler(document, 'click', e => {
+        if (!e.target.closest('.popup-content')) {
             requestAnimationFrame(popupHide);
         }
-    })
+    });
     handlerArray(popupBtn, 'click', () => {
         requestAnimationFrame(popupShow);
     });
@@ -139,29 +138,29 @@ const togglePopup = () => {
     });
 
 
-}
+};
 togglePopup();
 
 const scrollActive = () => {
     let countScroll = 0;
     let scrollAnimate;
-    const btnScroll = document.querySelector('a>img')
+    const btnScroll = document.querySelector('a>img');
     const main = document.querySelector('main');
 
     const scrollDown = () => {
-        countScroll = html.scrollTop
+        countScroll = html.scrollTop;
         if (html.scrollTop < main.scrollHeight) {
-            scrollAnimate = requestAnimationFrame(scrollDown)
+            scrollAnimate = requestAnimationFrame(scrollDown);
             countScroll += 20;
-            html.scrollTop = countScroll
+            html.scrollTop = countScroll;
         } else {
-            cancelAnimationFrame(scrollAnimate)
+            cancelAnimationFrame(scrollAnimate);
             countScroll = 0;
         }
-    }
+    };
 
-    handler(btnScroll, 'click', (e) => {
-        e.preventDefault()
+    handler(btnScroll, 'click', e => {
+        e.preventDefault();
         requestAnimationFrame(scrollDown);
     });
     handler(document, 'wheel', () => {
@@ -172,7 +171,7 @@ const scrollActive = () => {
         countScroll = 0;
         cancelAnimationFrame(scrollAnimate);
     });
-}
+};
 
 scrollActive();
 
